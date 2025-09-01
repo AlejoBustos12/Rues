7027a17f4b51d56867fb0c972c9e1addc2552198
DanielFAdarve <139408238+AlejoBustos12@users.noreply.github.com>
2025-08-31 20:31:42 -0500

    fix. se ajusta configuracion de cors

--- app/core/config.py
+++ app/core/config.py
@@ -1,21 +1,23 @@
-#Las configuraciones y cargas de datos del .env ademas de las variables globales
+# Las configuraciones y cargas de datos del .env ademas de las variables globales
 
 from pydantic import BaseModel
 from pydantic import field_validator
 from typing import List
 import os
 
+
 class Settings(BaseModel):
 
-    #Crear las propiedades de la clase
-    database_url : str = os.getenv("DATABASE_URL","")
-    api_key : str = os.getenv("DATABASE_URL","user-api-key")
-    cors_origin : List[str] = [o.strip() for o in os.getenv("CORS_ORIGIN","*").split(";")]
+    # Crear las propiedades de la clase
+    database_url: str = os.getenv("DATABASE_URL", "postgresql+psycopg2://postgres:1234@localhost:5432/rues")
+    api_key: str = os.getenv("DATABASE_URL", "user-api-key")
+    cors_origin: List[str] = [o.strip() for o in os.getenv("CORS_ORIGIN", "*").split(";")]
 
-    #cors   
-    @field_validator("cors_origins")
+    # cors
+    @field_validator("cors_origin")
     @classmethod
-    def normalize_origins(cls, v:List[str]) -> List[str]:
+    def normalize_origins(cls, v: List[str]) -> List[str]:
         return [o for o in v if o]
+
 
 settings = Settings()
--- app/main.py
+++ app/main.py
@@ -1,13 +1,36 @@
 from fastapi import FastAPI
 from app.api.v1 import router as api_router
 
+from fastapi.middleware.cors import CORSMiddleware
+from app.core.config import settings
+from app.db.database import Base, engine
+
 app = FastAPI(title="Api para consulta del RUES",version="1.0.0")
 
+Base.metadata.create_all(bind=engine)
 
+#CORS 
+if settings.cors_origin ==["*"]:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+
+    )
+else:
+       app.add_middleware(
+        CORSMiddleware,
+        allow_origins=settings.cors_origin,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+
+    )
 
 #Routers
 app.include_router(api_router)
-
 
 @app.get("/")
 async def health():
